c1ad2229132e9555c0ad0461e3dc0024a372a072
navendu.agarwal <navendu@sparshik.com>
2019-06-08 18:52:54 +0530

    switched to azure repo

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-docker build -t navendux/multi-client:latest -t navendux/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t navendux/multi-server:latest -t navendux/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t navendux/multi-worker:latest -t navendux/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t sparshik.azurecr.io/multi-client:latest -t sparshik.azurecr.io/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t sparshik.azurecr.io/multi-server:latest -t sparshik.azurecr.io/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t sparshik.azurecr.io/multi-worker:latest -t sparshik.azurecr.io/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
 # Take those images and push them to docker hub
-docker push navendux/multi-client:latest
-docker push navendux/multi-client:$GIT_SHA
-docker push navendux/multi-server:latest
-docker push navendux/multi-server:$GIT_SHA
-docker push navendux/multi-worker:latest
-docker push navendux/multi-worker:$GIT_SHA
+docker push sparshik.azurecr.io/multi-client:latest
+docker push sparshik.azurecr.io/multi-client:$GIT_SHA
+docker push sparshik.azurecr.io/multi-server:latest
+docker push sparshik.azurecr.io/multi-server:$GIT_SHA
+docker push sparshik.azurecr.io/multi-worker:latest
+docker push sparshik.azurecr.io/multi-worker:$GIT_SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=navendux/multi-server:$GIT_SHA
-kubectl set image deployments/client-deployment client=navendux/multi-client:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=navendux/multi-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=sparshik.azurecr.io/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=sparshik.azurecr.io/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=sparshik.azurecr.io/multi-worker:$GIT_SHA
